347652227e007969264c1bb42b5fbd1a45363b83
isaccanedo <isaccanedo@gmail.com>
2021-05-24 19:01:42 -0300

    :leaves: Initial commit

---
+++ src/main/java/com/isaccanedo/ThymeleafpostgresApplication.java
@@ -0,0 +1,13 @@
+package com.isaccanedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafpostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafpostgresApplication.class, args);
+	}
+
+}
